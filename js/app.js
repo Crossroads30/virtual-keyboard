96f70c2571e1461ae6e42b13efcecb05851b3f91
Crossroads30 <crossroads30@mail.ru>
2023-04-26 17:13:01 +0300

    feat: add keys to keyboard

--- js/app.js
+++ js/app.js
@@ -30,3 +30,19 @@ language.className = 'language';
 language.innerText = 'Для переключения языка комбинация: левыe ctrl + space';
 
 //create keyboard itself
+const keyPress = [96, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 45, 61, 81, 87, 69, 82, 84, 89, 85, 73, 79, 80, 91, 93, 13, 97, 115, 100, 102, 103, 104, 106, 107, 108, 59, 39, 92, 167, 122, 120, 99, 118, 98, 110, 109, 44, 46, 47, 32, 16];
+
+//document.onkeypress = function(event) {
+//  //console.log(event);
+//  keyPress.push(event.charCode);
+//  console.log(keyPress);
+//};
+function init() {
+  let out = '';
+  for (let i = 0; i < keyPress.length; i++) {
+    out += '<div class="keyboard-key">' + String.fromCharCode(keyPress[i]) + '</div>';
+  }
+  document.querySelector('.keyboard').innerHTML = out;
+}
+
+init()
